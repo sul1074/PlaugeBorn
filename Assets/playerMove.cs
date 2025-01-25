3e3872fe88fd980cf2911505dfec5a9f02ab9127
yj0602 <yj0602@pusan.ac.kr>
2025-01-26 00:01:44 +0900

    feat(playerMove.cs)
    회피용 대쉬기능 구현(근데 티가 잘 안나서 잔상이나 반투명 효과 추가 예정)

--- Assets/playerMove.cs
+++ Assets/playerMove.cs
@@ -8,6 +8,10 @@ public class Player : MonoBehaviour
     Rigidbody2D rigid;
     public float playerSpeed;
     private Animator animator;
+    public float dashSpeed; // 대쉬 속도
+    public float dashDuration; // 대쉬 지속 시간
+    private bool isDashing; // 대쉬 여부
+
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
@@ -16,9 +20,11 @@ public class Player : MonoBehaviour
 
     void Update()
     {   
-        // 플레이어 이동 (WASD)
-        inputVec.x = Input.GetAxisRaw("Horizontal");
-        inputVec.y = Input.GetAxisRaw("Vertical");
+        if (!isDashing)
+        {
+            // 플레이어 이동 (WASD)
+            inputVec.x = Input.GetAxisRaw("Horizontal");
+            inputVec.y = Input.GetAxisRaw("Vertical");
 
         if (inputVec.x != 0)
         {
@@ -31,11 +37,39 @@ public class Player : MonoBehaviour
         bool isMoving = inputVec.magnitude > 0;
         animator.SetFloat("RunState", isMoving ? 0.5f : 0f);    
         }
-
+        
+        if (Input.GetKeyDown(KeyCode.LeftShift) && !isDashing) // 왼쪽 쉬프트 누를 시 대쉬(회피)
+        {
+            StartCoroutine(Dash());
+        }
+    }
     private void FixedUpdate() 
     {   
-        Vector2 nextVec = inputVec.normalized * playerSpeed * Time.fixedDeltaTime;
-        // 위치 이동
-        rigid.MovePosition(rigid.position + nextVec);
+        if (!isDashing)
+        {    
+            Vector2 nextVec = inputVec.normalized * playerSpeed * Time.fixedDeltaTime;
+            // 위치 이동
+            rigid.MovePosition(rigid.position + nextVec);
+        }
+    }
+
+    IEnumerator Dash()
+    {
+        isDashing = true;
+        //animator.SetTrigger("Dash");
+
+        Vector2 dashDirection = inputVec.normalized;
+        if (dashDirection == Vector2.zero)
+        {
+            dashDirection = Vector2.right * Mathf.Sign(transform.localScale.x); // 일단 뒤로 가는 방향으로 설정해뒀어요
+        }
+
+        float startTime = Time.time;
+        while (Time.time < startTime + dashDuration)
+        {
+            rigid.MovePosition(rigid.position + dashDirection * dashSpeed * Time.fixedDeltaTime);
+            yield return null;
+        }
+        isDashing = false;
     }
 }
