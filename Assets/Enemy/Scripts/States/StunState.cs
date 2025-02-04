e4f7da3c92d5f41259cfdf2b2ab67a9bd3a2bd79
Vackam <pay2889@gmail.com>
2025-02-04 22:56:23 +0900

    feature(StunState) : 스턴 상태 구현

--- Assets/Enemy/Scripts/States/StunState.cs
+++ Assets/Enemy/Scripts/States/StunState.cs
@@ -4,17 +4,27 @@ using UnityEngine;
 
 public class StunState : IEnemyState
 {
+    [SerializeField]
+    private float WAIT_SECONDS = 2.0f;
     public void EnterState(IEnemyAI enemy)
     {
         Debug.Log("Entering Stun State"); // 로그
+        enemy.Animator.PlayAnimation(3);
+        MonoBehaviour enemyMB = enemy as MonoBehaviour;
+        // 되는 지 안되는 지 모름
+        // enemy.KnockBack();
+        enemyMB.StartCoroutine(WaitForNextState(enemy));
     }
     public void UpdateState(IEnemyAI enemy)
     {
-        enemy.Animator.PlayAnimation(3);
     }
 
     public void ExitState(IEnemyAI enemy)
     {
-        enemy.KnockBack();
+    }
+    private IEnumerator WaitForNextState(IEnemyAI enemy)
+    {
+        yield return new WaitForSeconds(WAIT_SECONDS);
+        enemy.ChangeState(new MoveState()); 
     }
 }
