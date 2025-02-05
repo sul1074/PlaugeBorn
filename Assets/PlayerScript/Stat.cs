ce58f4f3a448a053051eaae0159de68158218813
yj0602 <yj0602@pusan.ac.kr>
2025-02-05 18:01:27 +0900

    feat(StatReward.cs)
    기본 스탯 업그레이드 보상 (체력 증가, 방어력 증가, 데미지 증가, 이동속도 증가) 구현

--- Assets/PlayerScript/Stat.cs
+++ Assets/PlayerScript/Stat.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class Stat : MonoBehaviour
 {
-    [SerializeField] public float playerDamage;
-    [SerializeField] public float playerSkillDamage;
+    [SerializeField] public float playerDamage; // 평타 데미지
+    [SerializeField] public float playerSkillDamage; // 스킬 데미지
     [SerializeField] public float playerSpeed; // 이동 속도
     [SerializeField] public float playerHealth; // 플레이어 체력
-
+    [SerializeField] public float playerDefence; // 플레이어 방어력
 
 }
