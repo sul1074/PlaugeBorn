91f056684b33da28c44cb0f901f32e073f0b5898
Vackam <53655740+Vackam@users.noreply.github.com>
2025-02-07 14:12:47 +0900

    feature(IdleState.cs) : 겹치지 않도록 실행되게 변경

--- Assets/Enemy/Scripts/States/IdleState.cs
+++ Assets/Enemy/Scripts/States/IdleState.cs
@@ -7,6 +7,9 @@ public class IdleState : MonoBehaviour, IEnemyState
     // 해당 변수는 공격속도와 밀접한 연관을 가지고 있습니다.
     // 그러므로 적 개체별로 따로 할 수 있게 하든지,
     // 무슨 묘수가 필요할 듯
+
+    private Coroutine IdleCoroutine;
+
     [SerializeField]
     private float WAIT_SECONDS = 1.0f; 
     public void EnterState(IEnemyAI enemy)
@@ -14,14 +17,30 @@ public class IdleState : MonoBehaviour, IEnemyState
         Debug.Log("Entering Idle State"); // 로그
         enemy.Animator.PlayAnimation(0);
         MonoBehaviour enemyMB = enemy as MonoBehaviour;
-        enemyMB.StartCoroutine(WaitForNextState(enemy));
+
+        if (enemyMB != null)
+        {
+            if (IdleCoroutine != null)
+            {
+                enemyMB.StopCoroutine(IdleCoroutine);
+            } 
+        }
+        IdleCoroutine = enemyMB.StartCoroutine(WaitForNextState(enemy));
     }
     public void UpdateState(IEnemyAI enemy)
     {
     }
     public void ExitState(IEnemyAI enemy)
     {
-
+        if (IdleCoroutine != null)
+        {
+            MonoBehaviour enemyMB = enemy as MonoBehaviour;
+            if (enemyMB != null)
+            {
+                enemyMB.StopCoroutine(IdleCoroutine);
+            }
+            IdleCoroutine = null;
+        }
     }
 
     private IEnumerator WaitForNextState(IEnemyAI enemy)
