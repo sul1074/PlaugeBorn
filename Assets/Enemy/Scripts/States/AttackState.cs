8a687d03401611e316a362d1743f13ff0221ea7b
Vackam <53655740+Vackam@users.noreply.github.com>
2025-02-07 14:12:11 +0900

    feature(AttackState.cs) : 위치 수정

--- Assets/Enemy/Scripts/States/AttackState.cs
+++ Assets/Enemy/Scripts/States/AttackState.cs
@@ -7,11 +7,11 @@ public class AttackState : IEnemyState
     public void EnterState(IEnemyAI enemy)
     {
         Debug.Log("Entering Attack State"); // 로그
+        enemy.Animator.PlayAnimation(4);
+        enemy.AttackPlayer();
     }
     public void UpdateState(IEnemyAI enemy)
     {
-        enemy.Animator.PlayAnimation(4);
-
         enemy.ChangeState(new IdleState());
     }
     public void ExitState(IEnemyAI enemy)
