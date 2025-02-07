4eb7d3d71ce6ea379106b890acc8f6961e4ad2db
Vackam <53655740+Vackam@users.noreply.github.com>
2025-02-07 14:13:00 +0900

    feature(StunState.cs) : 스턴 겹치지 않도록 수정

--- Assets/Enemy/Scripts/States/StunState.cs
+++ Assets/Enemy/Scripts/States/StunState.cs
@@ -6,14 +6,24 @@ public class StunState : IEnemyState
 {
     [SerializeField]
     private float WAIT_SECONDS = 2.0f;
+
+    private Coroutine stunCoroutine;
     public void EnterState(IEnemyAI enemy)
     {
         Debug.Log("Entering Stun State"); // 로그
         enemy.Animator.PlayAnimation(3);
         MonoBehaviour enemyMB = enemy as MonoBehaviour;
-        // 되는 지 안되는 지 모름
-        // enemy.KnockBack();
-        enemyMB.StartCoroutine(WaitForNextState(enemy));
+
+        // When Enter the state, Check Coroutine.
+        if (enemyMB != null)
+        {
+            if (stunCoroutine != null)
+            {
+                enemyMB.StopCoroutine(stunCoroutine);
+            }
+            stunCoroutine = enemyMB.StartCoroutine(WaitForNextState(enemy));
+            enemy.KnockBack();
+        }
     }
     public void UpdateState(IEnemyAI enemy)
     {
@@ -21,6 +31,15 @@ public class StunState : IEnemyState
 
     public void ExitState(IEnemyAI enemy)
     {
+        // When Exit the State, Check Coroutine.
+        if (stunCoroutine != null) {
+            MonoBehaviour enemyMB = enemy as MonoBehaviour;
+            if (enemyMB != null)
+            {
+                enemyMB.StopCoroutine(stunCoroutine); // 상태 나갈 때 코루틴 정리
+            }
+            stunCoroutine = null;
+        }
     }
     private IEnumerator WaitForNextState(IEnemyAI enemy)
     {
