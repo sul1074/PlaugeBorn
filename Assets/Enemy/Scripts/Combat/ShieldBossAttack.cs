819244122ea263823673a106211246cbe34910a7
Vackam <53655740+Vackam@users.noreply.github.com>
2025-02-07 14:11:09 +0900

    feature(ShieldBossAttack.cs) : 방패적 공격 구현

---
+++ Assets/Enemy/Scripts/Combat/ShieldBossAttack.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldBossAttack : MonoBehaviour, IEnemyAttack
+{
+    public void Attack()
+    {
+
+    }
+}
