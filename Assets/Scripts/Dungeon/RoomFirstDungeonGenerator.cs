ea75bf11b992932edb63d005e669814e7fcdf61f
sul1074 <sul107485@gmail.com>
2025-02-08 22:10:18 +0900

    feat(RoomFirstDungeonGenerator.cs): 분할된 공간에서의 방을 랜덤한 형태로 생성하는 기능 추가
    RoomFirstDungeonGenerator.cs
    - 분할된 각 방들의 중심 좌표를 저장
    - 각 방의 중심 좌표에 대해, Random Walk를 수행해서 방을 랜덤한 형태로 생성.
    - 좌측, 우측, 바닥, 천장 벽에서 offset 만큼 뛰어서 생성

--- Assets/Scripts/Dungeon/RoomFirstDungeonGenerator.cs
+++ Assets/Scripts/Dungeon/RoomFirstDungeonGenerator.cs
@@ -33,7 +33,11 @@ public class RoomFirstDungeonGenerator : SimpleRandomWalkDungeonGenerator
 
         // 분할된 방들에 대해 타일을 채우기 위해, 방 내부 범위 안의 모든 좌표를 계산
         HashSet<Vector2Int> floor = new HashSet<Vector2Int>();
-        floor = CreateSimpleRooms(roomsList);
+
+        if (randomWalkRooms)
+            floor = CreateRoomsRandomly(roomsList);
+        else
+            floor = CreateSimpleRooms(roomsList);
 
         // 분할된 각 방의 중심 좌표를 저장
         List<Vector2Int> roomCenters = new List<Vector2Int>();
@@ -49,6 +53,62 @@ public class RoomFirstDungeonGenerator : SimpleRandomWalkDungeonGenerator
         // 방들에 대해 타일 채움
         tilemapVisualizer.PaintFloorTiles(floor);
         WallGenerator.CreateWalls(floor, tilemapVisualizer);
+    }
+
+    /// <summary>
+    /// 방을 랜덤한 형태로 생성
+    /// </summary>
+    private HashSet<Vector2Int> CreateRoomsRandomly(List<BoundsInt> roomsList)
+    {
+        HashSet<Vector2Int> floor = new HashSet<Vector2Int>();
+        for (int i = 0; i < roomsList.Count; i++)
+        {
+            BoundsInt roomsBounds = roomsList[i];
+
+            // 현재 방의 중심 좌표
+            Vector2Int roomCenter = new Vector2Int(Mathf.RoundToInt(roomsBounds.center.x), Mathf.RoundToInt(roomsBounds.center.y));
+
+            // 중심 좌표를 시작으로 Random Walk 수행해서 랜덤한 모양의 방 생성
+            HashSet<Vector2Int> roomFloor = RunRandomWalk(randomWalkParameters, roomCenter);
+
+            // 타일로 채워야 하는 방 내부의 모든 좌표를 계산
+            foreach (Vector2Int pos in roomFloor)
+            {
+                // 좌측, 우측, 바닥, 천장 벽에서 offset 만큼 뛰어서 생성
+                if ((pos.x >= roomsBounds.xMin + offset) && (pos.x <= roomsBounds.xMax - offset)
+                    && (pos.y >= roomsBounds.yMin + offset) && (pos.y <= roomsBounds.yMax - offset))
+                {
+                    floor.Add(pos);
+                }
+            }
+        }
+
+        return floor;
+    }
+
+    /// <summary>
+    /// 방을 정사각형 형태로 생성.
+    /// 각 방 내부에 대한 모든 좌표 정보를 계산해서 반환.
+    /// </summary>
+    private HashSet<Vector2Int> CreateSimpleRooms(List<BoundsInt> roomsList)
+    {
+        HashSet<Vector2Int> floor = new HashSet<Vector2Int>();
+
+        // 시작 좌표와 사이즈를 통해 방 내부의 모든 좌표 계산
+        foreach (BoundsInt room in roomsList)
+        {
+            // offset을 통해 각 방들의 간격이 띄어짐
+            for (int col = offset; col < room.size.x - offset; col++)
+            {
+                for (int row = offset; row < room.size.y - offset; row++)
+                {
+                    Vector2Int pos = (Vector2Int)room.min + new Vector2Int(col, row);
+                    floor.Add(pos);
+                }
+            }
+        }
+
+        return floor;
     }
 
     private HashSet<Vector2Int> ConnectRooms(List<Vector2Int> roomCenters)
@@ -119,7 +179,7 @@ public class RoomFirstDungeonGenerator : SimpleRandomWalkDungeonGenerator
         Vector2Int closest = Vector2Int.zero;
         float distance = float.MaxValue;
 
-        foreach(Vector2Int pos in roomCenters)
+        foreach (Vector2Int pos in roomCenters)
         {
             float currDistance = Vector2.Distance(pos, currentRoomCenter);
 
@@ -131,29 +191,5 @@ public class RoomFirstDungeonGenerator : SimpleRandomWalkDungeonGenerator
         }
 
         return closest;
-    }
-
-    /// <summary>
-    /// 각 방 내부에 대한 모든 좌표 정보를 계산해서 반환
-    /// </summary>
-    private HashSet<Vector2Int> CreateSimpleRooms(List<BoundsInt> roomsList)
-    {
-        HashSet<Vector2Int> floor = new HashSet<Vector2Int>();
-        
-        // 시작 좌표와 사이즈를 통해 방 내부의 모든 좌표 계산
-        foreach (BoundsInt room in roomsList)
-        {
-            // offset을 통해 각 방들의 간격이 띄어짐
-            for (int col = offset; col < room.size.x - offset; col++)
-            {
-                for (int row = offset; row < room.size.y - offset; row++) 
-                {
-                    Vector2Int pos = (Vector2Int)room.min + new Vector2Int(col, row);
-                    floor.Add(pos);
-                }
-            }
-        }
-
-        return floor;
     }
 }
