49957b66107cf806c10bbd638f576cbe4858afdc
yj0602 <yj0602@pusan.ac.kr>
2025-02-13 02:46:24 +0900

    feat(PlayerDamage.cs)
    데미지 계산 함수 구현

--- Assets/Player/PlayerAttack/PlayerDamage.cs
+++ Assets/Player/PlayerAttack/PlayerDamage.cs
@@ -3,32 +3,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerDamage : MonoBehaviour //데미지 = 공격력 × (100 / (100 + 적방어력))
+public class playerDamage : MonoBehaviour 
 {
     private Stat stat;
     private void Awake()
     {
         stat = GetComponent<Stat>();
     }
-    // 데미지 계산 함수
-    public float CalculateDamage(float enemyDefense)
-    {
-        float playerDamage = stat.playerATK * (100f / (100f + enemyDefense));
-        return Mathf.Max(playerDamage, 1); // 최소 데미지 1 보장
-    }
 
-    // 스킬 데미지 계산
-    public float CalculateSkillDamage(float enemyDefense)
+    /// <summary>
+    /// 데미지 계산 함수
+    /// </summary>
+    /// <param name="enemyDenfense">적 방어력</param>
+    /// <param name="skillMultiplier">스킬 배수(데미지 올릴 때)</param>
+    /// <returns> 데미지 = 공격력 × (100 / (100 + 적 방어력)) </returns>
+    public float CalculateDamage(float enemyDenfense, float skillMultiplier = 1f)
     {
-        float playerSkillDamage = stat.playerATK * 2 * (100f / (100f + enemyDefense));
-        return Mathf.Max(playerSkillDamage, 1);
-    }
-
-    // 벽력일섬 데미지 (예시로 방무 효과 추가)
-    public float CalculateLightningDamage(float enemyDefense)
-    {
-        float reduceDefense = enemyDefense * 0.8f;
-        float lightningDamage = stat.playerATK * 5 * (100f / (100f + reduceDefense));
-        return Mathf.Max(lightningDamage, 1);
+        float damage = stat.playerATK * skillMultiplier * (100f / (100f + enemyDenfense));
+        return Mathf.Max(damage, 1); // 최소 데미지 1 보장
     }
 }
--- Assets/Player/PlayerOpiton/PlayerStat.cs
+++ Assets/Player/PlayerOpiton/PlayerStat.cs
@@ -6,7 +6,7 @@ public class Stat : MonoBehaviour
 {
     [SerializeField] public float playerATK; // 플레이어 공격력
     [SerializeField] public float playerSpeed; // 이동 속도
-    [SerializeField] public float playerHealth; // 플레이어 체력
+    [SerializeField] public int playerHealth; // 플레이어 체력 (절반 하트 당 1로 계산)
     [SerializeField] public float playerDefence; // 플레이어 방어력
 }
 
