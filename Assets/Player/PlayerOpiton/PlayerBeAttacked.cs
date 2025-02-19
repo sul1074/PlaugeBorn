7a3dffbc39755237a33e01b3fc7d98a6d2bb1b12
yj0602 <yj0602@pusan.ac.kr>
2025-02-19 15:31:40 +0900

    feat(PlayerBeAttacked.cs)
    피격 시 효과 추가

--- Assets/Player/PlayerOpiton/PlayerBeAttacked.cs
+++ Assets/Player/PlayerOpiton/PlayerBeAttacked.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class PlayerBeAttacked : MonoBehaviour
 {
+    private SpriteRenderer[] spriteRenderers;
     private Player player;
     private Stat stat;
     private CapsuleCollider2D playerbody;
     // 레이어 마스크 필요 시 추가
+    private bool isDead = false;
 
     void Awake()
     {
@@ -15,18 +17,52 @@ public class PlayerBeAttacked : MonoBehaviour
         stat = GetComponent<Stat>();
         playerbody = GetComponent<CapsuleCollider2D>();
         playerbody.enabled = true;
+        spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
     }
 
     private void OnTriggerEnter2D(Collider2D other) { // EnemyAttack 태그에 맞았을 때 피격되는 것으로 일단 구현
         if (other.CompareTag("EnemyAttack")) {
+            if (isDead) return;
             stat.playerHealth -= 1;
             Debug.Log("피격, 체력 -1 감소");
-            return;
+            
+            if (stat.playerHealth <= 0)
+            {
+                player.Die();
+                isDead = true;
+                return;
+            }
+            StartCoroutine(BlinkEffect(2, 0.05f));
         }
-        if (stat.playerHealth <= 0)
+    }
+/// <summary>
+/// 피격 시 효과 함수
+/// </summary>
+/// <param name="blinkCount"></param>
+/// <param name="blinkInterval"></param>
+/// <returns>깜박임 횟수, 간격 설정</returns>
+    private IEnumerator BlinkEffect(int blinkCount, float blinkInterval)
+    {
+        for (int i = 0; i < blinkCount; i++)
         {
-            player.Die();
-            return;
+            SetAlpha(0.3f);
+            yield return new WaitForSeconds(blinkInterval);
+            SetAlpha(1f);
+            yield return new WaitForSeconds(blinkInterval);
+        }
+    }
+
+// 피격 투명도 변화 함수
+    private void SetAlpha(float alpha)
+    {
+        foreach (var sprite in spriteRenderers)
+        {
+            if (sprite.CompareTag("Attack")) continue;
+            if (sprite != null) {
+            Color color = sprite.color;
+            color.a = alpha;
+            sprite.color = color;
+            }
         }
     }
 
