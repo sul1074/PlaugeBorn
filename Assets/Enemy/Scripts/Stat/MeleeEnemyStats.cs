fbd3ca2e9207219433b1b93956d702e66509f7c7
Vackam <53655740+Vackam@users.noreply.github.com>
2025-02-21 14:20:56 +0900

    feature(MeleeEnemyStats.cs) : 죽음 및 스턴 연계

--- Assets/Enemy/Scripts/Stat/MeleeEnemyStats.cs
+++ Assets/Enemy/Scripts/Stat/MeleeEnemyStats.cs
@@ -18,12 +18,25 @@ public class MeleeEnemyStats : MonoBehaviour, IEnemyStats
         //// Change State to Stun State 
         //GetComponent<MeleeEnemyAI>().ChangeState(new StunState());
 
-        // TODO: TEST CODE
-        GetComponent<MeleeEnemyAI>().ChangeState(new DashState());
+        CheckDie();
+        GetComponent<MeleeEnemyAI>().ChangeState(new StunState());
     }
 
     private void SubHp(float damage)
     {
         this.Hp -= damage;
+    }
+
+    private void CheckDie()
+    {
+        if (this.Hp <= 0)
+        {
+            Rigidbody2D rb = GetComponent<Rigidbody2D>();
+            rb.velocity = Vector2.zero;
+            IEnemyAI ai = GetComponent<IEnemyAI>();
+            // 블락 차단 무조건 진입할 수 있게
+            ai.IsBlocking = false;
+            ai.ChangeState(new DeadState());
+        }
     }
 }
