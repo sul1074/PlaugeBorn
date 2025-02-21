f190d1440f264eef1d9d997ba821b0f8e7ba2e6d
Vackam <53655740+Vackam@users.noreply.github.com>
2025-02-21 14:22:46 +0900

    feature(DeadState.cs) : 죽음 상태 구현

---
+++ Assets/Enemy/Scripts/States/DeadState.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class DeadState : MonoBehaviour, IEnemyState 
+{
+    public void EnterState(IEnemyAI enemy)
+    {
+        Debug.Log("Entering Dead State"); // 로그
+        enemy.Animator.PlayAnimation(2);
+        MonoBehaviour enemyMB = enemy as MonoBehaviour;
+        if (enemyMB != null)
+        {
+            Debug.Log("아니 이거 왜 안됨?");
+            Destroy(enemyMB.gameObject, 2.0f);
+        }
+    }
+    public void UpdateState(IEnemyAI enemy)
+    {
+    }
+    public void ExitState(IEnemyAI enemy)
+    {
+    }
+}
