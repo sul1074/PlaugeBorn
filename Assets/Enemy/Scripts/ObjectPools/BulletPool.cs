8806b708613938326575f16292f44394068ab1f2
Vackam <53655740+Vackam@users.noreply.github.com>
2025-02-21 14:20:25 +0900

    feature(BulletPool.cs) : 오브젝트 풀 구현

---
+++ Assets/Enemy/Scripts/ObjectPools/BulletPool.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class BulletPool : MonoBehaviour
+{
+    public static BulletPool Instance { get; private set; }
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private int initialPoolSize = 500;
+    
+    private Queue<GameObject> pool = new Queue<GameObject>();
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        for (int i = 0; i < initialPoolSize; i++)
+        {
+            GameObject bullet = Instantiate(bulletPrefab);
+            bullet.SetActive(false);
+            pool.Enqueue(bullet);
+        }
+    }
+
+    public GameObject GetBullet()
+    {
+        if (pool.Count > 0)
+        {
+            GameObject bullet = pool.Dequeue();
+            bullet.SetActive(true);
+            return bullet;
+        }
+        else
+        {
+            GameObject bullet = Instantiate(bulletPrefab);
+            return bullet;
+        }
+    }
+
+    public void ReturnBullet(GameObject bullet)
+    {
+        bullet.SetActive(false);
+        pool.Enqueue(bullet);
+    }
+}
