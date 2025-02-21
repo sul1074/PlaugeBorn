218ffe00a492bfe9357dddcaf193371ce7b446e0
Vackam <53655740+Vackam@users.noreply.github.com>
2025-02-21 14:12:39 +0900

    feature(ShieldBossEnemyAI.cs) : 보스 적 랜덤 패턴 구현 및 죽음 구현

--- Assets/Enemy/Scripts/AI/ShieldBossEnemyAI.cs
+++ Assets/Enemy/Scripts/AI/ShieldBossEnemyAI.cs
@@ -9,8 +9,13 @@ public class ShieldBossEnemyAI : MonoBehaviour, IEnemyAI
     public float AttackRange = 3f;
     public LayerMask playerLayer;
 
-    [Header("공격 판정 변수")]
-    private float DashRange = 3.0f;
+    [Header("공격 판정 변수")] private float DashRange = 3.0f;
+    
+    [Header("가중치 변수")]
+    private float shieldWeight = 1f;
+    private float dashWeight = 2f;
+    private float sprialWeight = 0.5f;
+
 
     // 공격 판정을 위한 플레이어 찾기
     private Transform playerPos;
@@ -39,6 +44,11 @@ public class ShieldBossEnemyAI : MonoBehaviour, IEnemyAI
 
     public void ChangeState(IEnemyState newState)
     {
+        if (currentState is DeadState)
+        {
+            Debug.Log("Dead");
+            return;
+        }
         if (IsBlocking)
         {
             Debug.LogWarning("Now In Charging Attack");
@@ -51,20 +61,28 @@ public class ShieldBossEnemyAI : MonoBehaviour, IEnemyAI
 
     public void AttackPlayer()
     {
-        float range = (playerPos.position - transform.position).magnitude;
-        if (range > 3f)
+        ExecuteRandomPattern();
+    }
+    public void ExecuteRandomPattern()
+    {
+        ShieldBossAttack attack = GetComponent<ShieldBossAttack>();
+        float totalWeight = shieldWeight + dashWeight + sprialWeight; 
+        float randomValue = UnityEngine.Random.Range(0f, totalWeight);
+
+        if (randomValue < shieldWeight)
         {
-            IsBlocking = true;
-            transform.GetComponent<ShieldBossAttack>().Attack();
+            // TODO: 매직넘버 지우기
+            attack.ShieldAttack(1.5f);        
+        }
+        else if (randomValue < shieldWeight + dashWeight)
+        {
+            attack.Attack(); 
         }
         else
         {
-            IsBlocking = true;
-            transform.GetComponent <ShieldBossAttack>().ShieldAttack();
+            attack.SpiralAttack();
         }
     }
-
-
     public bool IsPlayerInAttackRange()
     {
         Collider2D[] hits = Physics2D.OverlapCircleAll(transform.position, AttackRange, playerLayer);
