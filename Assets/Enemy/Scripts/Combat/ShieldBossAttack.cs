f6efb0872988cd20acae64d2b9955ff6826b1800
Vackam <53655740+Vackam@users.noreply.github.com>
2025-02-21 14:12:59 +0900

    feature(ShieldBossAttack.cs) : 나선 공격 구현

--- Assets/Enemy/Scripts/Combat/ShieldBossAttack.cs
+++ Assets/Enemy/Scripts/Combat/ShieldBossAttack.cs
@@ -1,35 +1,38 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class ShieldBossAttack : MonoBehaviour, IEnemyAttack
 {
-    [SerializeField] private GameObject ShieldProjectile;
+    [FormerlySerializedAs("ShieldProjectile")] [SerializeField] private GameObject shieldProjectile;
     [Header("Four Direction")]
-    private Vector2[] cardinalPoints = new Vector2[]
-    {
+    private readonly Vector2[] cardinalPoints = {
         Vector2.up,
         Vector2.down,
         Vector2.left,
         Vector2.right
     };
 
-    private Vector2[] diagonalPoints = new Vector2[]
-    {
+    private readonly Vector2[] diagonalPoints = {
         new Vector2(1, 1).normalized,
         new Vector2(-1, 1).normalized,
         new Vector2(1, -1).normalized,
         new Vector2(-1, -1).normalized
     };
 
+    [Header("방패 저장소")]
+    [SerializeField]
+    private List<GameObject> shieldList = new List<GameObject>();
     [Header("방패 속도 설정")]
     private float shieldSpeed = 5.0f;
     private float shieldDuration = 2.0f;
 
     [Header("돌진 속도 설정")]
-    private float dashSpeed = 3.0f;
-    private float dashWaitingDuration = 2.0f;
-    private float dashDuration = 1.0f;
+    private float dashSpeed = 10.0f;
+    private float dashWaitingDuration = 1.0f;
+    private float dashDuration = 0.5f;
     private Rigidbody2D EnemyRb;
 
     [Header("경고선 설정")]
@@ -37,7 +40,12 @@ public class ShieldBossAttack : MonoBehaviour, IEnemyAttack
     public float lineLength;
     public float waitingDuration = 1.5f;
     private List<GameObject> warningLines = new List<GameObject>();
-
+    
+    [Header("나선 공격 변수")]
+    private float angle = 0f;
+    private const float ONE_EIGHTY = 180f;
+    private const float PLUS_DEGREE = 10f;
+    
     // TODO: 살짝 답답하군
     public void Start()
     {
@@ -50,39 +58,61 @@ public class ShieldBossAttack : MonoBehaviour, IEnemyAttack
         StartCoroutine(ExecuteDashAttackAfterDelay());
     }
 
-    public void ShieldAttack()
+    public void ShieldAttack(float duration)
     {
-        StartCoroutine(TotalShieldAttack());
+        StartCoroutine(TotalShieldAttack(duration));
+    }
+
+    public void SpiralAttack()
+    {
+        StartCoroutine(ExecuteSpiralAttack());
     }
 
     // 방패 던지기 패턴
 
-    private IEnumerator TotalShieldAttack()
+    private IEnumerator TotalShieldAttack(float duration)
     {
+        ShieldBossEnemyAI ai = GetComponent<ShieldBossEnemyAI>();
+        ai.IsBlocking = true;
         yield return StartCoroutine(ExecuteShieldAttackAfterDelay(cardinalPoints));
         // TODO: 매직넘버 지우기
-        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(duration);
         yield return StartCoroutine(ExecuteShieldAttackAfterDelay(diagonalPoints));
 
-        GetComponent<ShieldBossEnemyAI>().IsBlocking = false;
+        yield return new WaitUntil(() => (shieldList.Count == 0) && (warningLines.Count == 0));
+
+        ai.IsBlocking = false;
+        ai.ChangeState(new MoveState()); 
     }
     private void ShieldPattern(Vector2[] Points)
     {
         foreach (var point in Points)
         {
-            GameObject Shield = Instantiate(ShieldProjectile, transform);
+            GameObject Shield = Instantiate(shieldProjectile, transform);
+            shieldList.Add(Shield);
             Shield.SetActive(true);
             ShieldProjectileAttack temp = Shield.GetComponent<ShieldProjectileAttack>();
+            temp.SetBossAttack(this);
             temp.SetDir(point);
             temp.StartCoroutine(temp.ShieldAttack());
         }
     }
 
+    public void RemoveShield(GameObject Shield)
+    {
+        shieldList.Remove(Shield);
+    }
+
+    /// <summary>
+    /// Creates and displays warning lines from the object's position in the specified directions.
+    /// </summary>
+    /// <param name="Points">An array of direction vectors for each warning line.</param>
+    /// <param name="lineLength">The length to extend each line from the starting position.</param>
     private void ShowWarningLines(Vector2[] Points, float lineLength)
     {
         if (Points.Length == 0 || Points == null)
         {
-            // 예외처리
+            // Exception
             Debug.Log("No Points to make Warning Lines");
             return;
         }
@@ -92,7 +122,7 @@ public class ShieldBossAttack : MonoBehaviour, IEnemyAttack
         {
             GameObject lineObj = Instantiate(redLinePrefab, startPosition, Quaternion.identity);
             LineRenderer lr = lineObj.GetComponent<LineRenderer>();
-            if (lr != null )
+            if (lr)
             {
                 lr.SetPosition(0, startPosition);
                 lr.SetPosition(1, startPosition + dir * lineLength);
@@ -105,12 +135,7 @@ public class ShieldBossAttack : MonoBehaviour, IEnemyAttack
     {
         ShowWarningLines(Points, this.lineLength);
         yield return new WaitForSeconds(waitingDuration);
-        
-        foreach (GameObject line in warningLines)
-        {
-            Destroy(line);
-        }
-        warningLines.Clear();
+        ClearWarningLines();
 
         ShieldPattern(Points);
     }
@@ -126,27 +151,85 @@ public class ShieldBossAttack : MonoBehaviour, IEnemyAttack
         else
         {
             Debug.Log("No Points to make Warning Lines");
-            return;
         }
     }
 
     private IEnumerator ExecuteDashAttackAfterDelay()
     {
+        ShieldBossEnemyAI ai = GetComponent<ShieldBossEnemyAI>();
+        ai.IsBlocking = true;
         // 편법으로 하기.
         Vector2[] dir = new Vector2[] { GetComponent<ShieldBossEnemyMovement>().GetDirection().normalized };
         ShowWarningLines(dir, dashDuration * dashSpeed);
         yield return new WaitForSeconds(dashWaitingDuration);
 
-        foreach (GameObject line in warningLines)
-        {
-            Destroy(line);
-        }
-        warningLines.Clear();
+        ClearWarningLines();
 
         DashPattern(dir);
         yield return new WaitForSeconds(dashDuration);
         EnemyRb.velocity = Vector3.zero;
 
-        GetComponent<ShieldBossEnemyAI>().IsBlocking = false;
+        ai.IsBlocking = false;
+        ai.ChangeState(new MoveState()); 
+    }
+
+    /// <summary>
+    /// Destroy All GameObject in warningLines
+    /// </summary>
+    private void ClearWarningLines()
+    {
+        foreach(GameObject line in warningLines)
+        {
+            Destroy(line);
+        }
+        warningLines.Clear();
+    }
+    
+    // 나선 패턴
+        
+    private IEnumerator ExecuteSpiralAttack()
+    {
+        ShieldBossEnemyAI ai = GetComponent<ShieldBossEnemyAI>();
+        ai.IsBlocking = true;
+        // TODO: 매직넘버 지우기
+        for (int i = 0; i < 20; i++)
+        {
+            yield return new WaitForSeconds(0.1f);
+            SpiralFirePattern(angle);
+            SpiralFirePattern(angle + ONE_EIGHTY / 2);
+            SpiralFirePattern(angle + ONE_EIGHTY);
+            SpiralFirePattern(angle + ONE_EIGHTY + ONE_EIGHTY / 2);
+            
+            angle += PLUS_DEGREE;
+        }
+        // 반대로 ㄱㄱ
+        for (int i = 0; i < 20; i++)
+        {
+            yield return new WaitForSeconds(0.1f);
+            SpiralFirePattern(angle + ONE_EIGHTY + ONE_EIGHTY / 2);
+            SpiralFirePattern(angle + ONE_EIGHTY);
+            SpiralFirePattern(angle + ONE_EIGHTY / 2);
+            SpiralFirePattern(angle);
+            
+            angle -= PLUS_DEGREE;
+        }
+        ai.IsBlocking = false;
+        angle = 0f;
+        ai.ChangeState(new MoveState()); 
+    }
+
+    private void SpiralFirePattern(float angle)
+    {
+        float bulDirX = transform.position.x + Mathf.Sin((angle * Mathf.PI) / ONE_EIGHTY);
+        float bulDirY = transform.position.y + Mathf.Cos((angle * Mathf.PI) / ONE_EIGHTY);
+        
+        Vector3 bulMoveVector = new Vector3(bulDirX, bulDirY, 0);
+        Vector2 bulDir = (bulMoveVector - transform.position).normalized;
+
+        GameObject bul = BulletPool.Instance.GetBullet();
+        bul.transform.position = transform.position;
+        bul.transform.rotation = transform.rotation;
+        bul.SetActive(true);
+        bul.GetComponent<SpiralBulletAttack>().SetMoveDirection(bulDir);
     }
 }
