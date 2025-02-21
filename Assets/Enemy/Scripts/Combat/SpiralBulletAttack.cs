cbca56d6e5453c70e8a9a1a562b28d9de9ed8f50
Vackam <53655740+Vackam@users.noreply.github.com>
2025-02-21 14:18:37 +0900

    feature(SpiralBulletAttack.cs) : 나선 공격 패턴 구현

---
+++ Assets/Enemy/Scripts/Combat/SpiralBulletAttack.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+public class SpiralBulletAttack : MonoBehaviour
+{
+    private Vector2 moveDirection;
+    private float moveSpeed = 5f;
+
+    private void OnEnable()
+    {
+        // TODO: 매직넘버 지우기
+        Invoke("Destroy", 8f);
+    }
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        transform.Translate(moveDirection * (moveSpeed * Time.deltaTime));
+    }
+
+    public void SetMoveDirection(Vector2 moveDirection)
+    {
+        this.moveDirection = moveDirection;
+    }
+
+    private void Destroy()
+    {
+        BulletPool.Instance.ReturnBullet(this.GameObject());
+    }
+}
