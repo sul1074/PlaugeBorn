4d4005cf477566c37ecb8e59824794d24ea7654e
Vackam <53655740+Vackam@users.noreply.github.com>
2025-02-21 14:21:28 +0900

    feature(ShieldBossEnemyStats.cs) : 죽음 구현

--- Assets/Enemy/Scripts/Stat/ShieldBossEnemyStats.cs
+++ Assets/Enemy/Scripts/Stat/ShieldBossEnemyStats.cs
@@ -12,9 +12,23 @@ public class ShieldBossEnemyStats : MonoBehaviour, IEnemyStats
         SubHp(finalDamage);
         
         // 보스는 스턴 없이 진행
+        CheckDie();
+        GetComponent<ShieldBossEnemyAI>().ChangeState(new MoveState());
     }
     private void SubHp(float damage)
     {
         this.Hp -= damage;
+    }
+    private void CheckDie()
+    {
+        if (this.Hp <= 0)
+        {
+            Rigidbody2D rb = GetComponent<Rigidbody2D>();
+            rb.velocity = Vector2.zero;
+            IEnemyAI ai = GetComponent<IEnemyAI>();
+            // 블락 차단 무조건 진입할 수 있게
+            ai.IsBlocking = false;
+            ai.ChangeState(new DeadState());
+        }
     }
 }
