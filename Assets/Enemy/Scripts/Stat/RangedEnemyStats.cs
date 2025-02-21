8545d68a45c9c3b9417446b6669d8765178d591e
Vackam <53655740+Vackam@users.noreply.github.com>
2025-02-21 14:21:09 +0900

    feature(RangedEnemyStats.cs) : 죽음 및 스턴 연계

--- Assets/Enemy/Scripts/Stat/RangedEnemyStats.cs
+++ Assets/Enemy/Scripts/Stat/RangedEnemyStats.cs
@@ -14,11 +14,24 @@ public class RangedEnemyStats : MonoBehaviour, IEnemyStats
         float finalDamage = damage * (100.0f - Defence) / 100.0f;
         SubHp(finalDamage);
 
+        CheckDie();
         GetComponent<RangedEnemyAI>().ChangeState(new StunState());
     }
 
     private void SubHp(float damage)
     {
         this.Hp -= damage;
+    }
+    private void CheckDie()
+    {
+        if (this.Hp <= 0)
+        {
+            Rigidbody2D rb = GetComponent<Rigidbody2D>();
+            rb.velocity = Vector2.zero;
+            IEnemyAI ai = GetComponent<IEnemyAI>();
+            // 블락 차단 무조건 진입할 수 있게
+            ai.IsBlocking = false;
+            ai.ChangeState(new DeadState());
+        }
     }
 }
